39463e4ecf3aa9d8d7c4c4e2b11bc85a1af0e1d8
Steve Niles <steve.niles@gmail.com>
2012-06-27 23:49:11 -0400

    Organized using statements

--- Simple Image Resizer/Form1.cs
+++ Simple Image Resizer/Form1.cs
@@ -1,16 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using System.IO;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
-using Simple_Image_Resizer.Extensions;
+using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using Simple_Image_Resizer.Extensions;
 
 namespace Simple_Image_Resizer
 {
